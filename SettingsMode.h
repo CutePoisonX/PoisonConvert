06296ea608a3290ba09959ae2c25f76bac7b23ee
CutePoisonX <CutePoisonXI@gmail.com>
2014-09-25 15:17:14 +0200

    Finalized version 1.4

--- SettingsMode.h
+++ SettingsMode.h
@@ -1,9 +1,21 @@
-/* 
- * File:   SettingsMode.h
- * Author: CutePoisonX
- *
- * Created on 08. Februar 2013, 09:40
- */
+//
+//    Copyright 2014 CutePoisonX (CutePoisonXI@gmail.com)
+//
+//    This file is part of PoisonConvert.
+//
+//    PoisonConvert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    PoisonConvert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with PoisonConvert.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef SETTINGSMODE_H
 #define	SETTINGSMODE_H
